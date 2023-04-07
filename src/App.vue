a9ce05ee0097dc43ed1a22affefef9dc6ec751f2
fuhao <632951357@qq.com>
2023-04-07 17:07:57 +0800

    feat: 新增 沸点养鱼计划第四期 进度提示功能

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
 import { onMounted, reactive, ref, watch } from 'vue';
 import { initTouch } from './hooks/useTouchBtn';
-import { ElForm, ElFormItem, ElSwitch } from 'element-plus';
+import { ElForm, ElFormItem, ElSwitch, ElPopover, ElLink } from 'element-plus';
 import { usePinsBlackPlugin } from './plugin/usePinsBlackPlugin';
 import { coralReadingRewards } from './plugin/coralReadingRewards';
 import { useHideHotListPlugin } from './plugin/useHideHotListPlugin';
+import { ClockInData, useFishPlanPlugin } from './plugin/useFishPlan';
 
 let pinsBlackObj = usePinsBlackPlugin();
 let cralReadingRewardsObj = coralReadingRewards();
 let hideHotListPlugin = useHideHotListPlugin();
+let fishPlanPlugin = useFishPlanPlugin();
 
 let defaultFormData = {
   pinsBlackState: false,
   blackKeyList: ['星期四'],
   coralReadingRewardsState: false,
   hideHotListState: false,
+  fishPlanState: true,
 };
 // 处理入口icon
 let state = reactive<{
@@ -24,6 +27,7 @@ let state = reactive<{
   open: boolean;
   moveState: boolean;
   formData: typeof defaultFormData;
+  clockInData: ClockInData[];
 }>({
   top: 150,
   left: 10,
@@ -33,6 +37,7 @@ let state = reactive<{
   open: false,
   moveState: false,
   formData: JSON.parse(localStorage.getItem('fromData') || JSON.stringify(defaultFormData)),
+  clockInData: [],
 });
 let iconContainer = ref<HTMLDivElement>();
 
@@ -95,50 +100,36 @@ watch(
   },
   { immediate: true }
 );
+
+watch(
+  () => state.formData.fishPlanState,
+  async (val) => {
+    if (val) {
+      state.clockInData = await fishPlanPlugin.start();
+    } else {
+      fishPlanPlugin.stop();
+    }
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
-  <div
-    class="container"
-    ref="iconContainer"
-    :style="{ '--top': `${state.top}px`, '--left': `${state.left}px` }"
-  >
+  <div class="container" ref="iconContainer" :style="{ '--top': `${state.top}px`, '--left': `${state.left}px` }">
     <div class="icon-container" @click="showContent">
-      <img
-        class="icon"
-        :draggable="false"
-        src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/6c61ae65d1c41ae8221a670fa32d05aa.svg"
-      />
+      <img class="icon" :draggable="false"
+        src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/6c61ae65d1c41ae8221a670fa32d05aa.svg" />
     </div>
-    <div
-      class="panel"
-      @click.stop="false"
-      @mousedown.stop="false"
-      @touchstart.stop="false"
-      v-if="state.open"
-      :style="{ [`${state.panelState.position}`]: 0 }"
-    >
+    <div class="panel" @click.stop="false" @mousedown.stop="false" @touchstart.stop="false" v-if="state.open"
+      :style="{ [`${state.panelState.position}`]: 0 }">
       <ElForm :model="state.formData">
         <ElFormItem label="沸点关键字屏蔽">
           <ElSwitch v-model="state.formData.pinsBlackState"></ElSwitch>
         </ElFormItem>
         <ElFormItem label="关键字列表">
-          <ElSelect
-            class="hidden-popper"
-            v-model="state.formData.blackKeyList"
-            multiple
-            filterable
-            allow-create
-            default-first-option
-            :reserve-keyword="false"
-            :teleported="false"
-          >
-            <ElOption
-              v-for="item in state.formData.blackKeyList"
-              :key="item"
-              :label="item"
-              :value="item"
-            ></ElOption>
+          <ElSelect class="hidden-popper" v-model="state.formData.blackKeyList" multiple filterable allow-create
+            default-first-option :reserve-keyword="false" :teleported="false">
+            <ElOption v-for="item in state.formData.blackKeyList" :key="item" :label="item" :value="item"></ElOption>
           </ElSelect>
         </ElFormItem>
         <ElFormItem label="会员文章阅读任务">
@@ -147,6 +138,32 @@ watch(
         <ElFormItem label="隐藏榜单">
           <ElSwitch v-model="state.formData.hideHotListState"></ElSwitch>
         </ElFormItem>
+        <ElFormItem label="沸点养鱼计划第四期">
+          <template #label>
+            <a href="https://juejin.cn/post/7216213764776984633">沸点养鱼计划第四期</a>
+          </template>
+          <ElSwitch v-model="state.formData.fishPlanState"></ElSwitch>
+        </ElFormItem>
+        <template v-if="state.formData.fishPlanState">
+          <ElFormItem label="签到日历"> </ElFormItem>
+          <div class="fishList">
+            <el-popover v-for="item in state.clockInData" :key="item.dateStr" placement="top-start" title="去打卡" :width="100" trigger="hover" popper-class="fish__tip"
+              >
+              <div class="link__list">
+                <el-link :href="`https://juejin.cn/pin/club/${item.id}`" target="_blank" v-for="item2 in item.topicList">{{ item2.name }}</el-link>
+              </div>
+              <template #reference>
+                <div class="fishItem">
+                  <div>{{ item.dateStr }}</div>
+                  <div>{{ item.festival }}</div>
+                  <div v-if="item.check === 1" class="fish__state active">已打卡</div>
+                  <div v-else-if="item.check === -1" class="fish__state deactive">未打卡</div>
+                  <div class="fish__state" v-else>待打卡</div>
+                </div>
+              </template>
+            </el-popover>
+          </div>
+        </template>
       </ElForm>
     </div>
   </div>
@@ -157,7 +174,7 @@ watch(
   position: fixed;
   top: var(--top);
   left: var(--left);
-  z-index: 99999;
+  z-index: 2058;
   width: auto;
 }
 
@@ -181,7 +198,7 @@ watch(
 .panel {
   position: absolute;
   background-color: #ffffff;
-  width: 300px;
+  width: 350px;
   padding: 10px;
   border-radius: 10px;
   box-shadow: 0px 0px 10px 0px rgba(113, 151, 221, 0.3);
@@ -194,5 +211,40 @@ watch(
 
 ::v-deep(.hidden-popper .el-popper) {
   display: none;
+}
+
+.fishList {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(4, 50px);
+  grid-row-gap: 10px;
+}
+
+.fishItem {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.fish__state {}
+
+.fish__state.active {
+  color: #409eff;
+}
+
+.fish__state.deactive {
+  color: #ff0000;
+}
+.link__list{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+</style>
+
+<style>
+.el-popper.fish__tip{
+  z-index: 2059 !important;
 }
 </style>
---
+++ src/plugin/useFishPlan.ts
@@ -0,0 +1,195 @@
+import { unsafeWindow } from '$';
+import dayjs from 'dayjs';
+import { globalFetch } from '../hooks/useFetch';
+
+let activeData = [
+  {
+    id: 1,
+    date: ['2023-04-03', '2023-04-09'],
+    topicList: [
+      {
+        id: '6824710202248396813',
+        name: '读书会',
+      },
+      {
+        id: '6824710202873348104',
+        name: '打工人的日常',
+      },
+    ],
+  },
+  {
+    id: 2,
+    date: ['2023-04-10', '2023-04-16'],
+    topicList: [
+      {
+        id: '6931179346187321351',
+        name: '理财交流圈',
+      },
+      {
+        id: '6931914335023267853',
+        name: '游戏玩家俱乐部',
+      },
+    ],
+  },
+  {
+    id: 3,
+    date: ['2023-04-17', '2023-04-23'],
+    topicList: [
+      {
+        id: '6931915139369140224',
+        name: '搞笑段子',
+      },
+      {
+        id: '6824710202562969614',
+        name: '今天学到了',
+      },
+    ],
+  },
+  {
+    id: 4,
+    date: ['2023-04-24', '2023-04-30'],
+    topicList: [
+      {
+        id: '6824710202302922759',
+        name: '舌尖上的沸点'
+      },
+      {
+        id: '6824710203464761352',
+        name: '今日新鲜事'
+      }
+    ],
+  },
+];
+
+export type TopicItem = {
+  id: string;
+  name: string;
+}
+export type ClockInData = {
+  dateUnix: number;
+  dateStr: string;
+  topicList: TopicItem[];
+  festival: string;
+  check: number;
+};
+
+let festivalData = [
+  '立春',
+  '雨水',
+  '清明',
+  '惊蛰',
+  '春分',
+  '谷雨',
+  '春季补签',
+  '立夏',
+  '小满',
+  '芒种',
+  '夏至',
+  '小暑',
+  '大暑',
+  '夏季补签',
+  '立秋',
+  '处暑',
+  '白露',
+  '秋分',
+  '寒露',
+  '霜降',
+  '秋季补签',
+  '立冬',
+  '小雪',
+  '大雪',
+  '冬至',
+  '小寒',
+  '大寒',
+  '冬季补签',
+];
+
+export function useFishPlanPlugin() {
+  let _startDate = dayjs('2023-04-03').startOf('day');
+  let todayUnix = dayjs().startOf('day').unix()
+  let clockInData: ClockInData[] = activeData.flatMap((v) => {
+    let [startData, endData] = v.date;
+    let startDate = dayjs(startData).startOf('day');
+    let endDate = dayjs(endData).startOf('day');
+    let list = [];
+    for (; startDate.unix() <= endDate.unix(); startDate = startDate.add(1, 'day')) {
+      list.push({
+        dateUnix: startDate.unix(),
+        dateStr: startDate.format('MM.DD'),
+        topicList: [...v.topicList.map(v=>Object.assign({}, v))],
+        festival: festivalData[startDate.diff(_startDate, 'day')],
+        check: todayUnix > startDate.unix() ? -1 : 0,
+      });
+    }
+    return list;
+  });
+
+  let aid = unsafeWindow.TEAVisualEditor.appId;
+  let cacheToken = JSON.parse(localStorage.getItem(`__tea_cache_tokens_${aid}`) || '{}');
+  let userUniqueId = cacheToken['user_unique_id'];
+  let slardarData = JSON.parse(localStorage.getItem(`SLARDAR${aid}`) || '{}');
+  let cursor = 0;
+
+  function getLsit(cursor: number) {
+    return globalFetch
+      .request(
+        `https://api.juejin.cn/content_api/v1/short_msg/query_list?aid=${aid}&uuid=${userUniqueId}&spider=0`,
+        {
+          headers: {
+            accept: '*/*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'content-type': 'application/json',
+            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'x-secsdk-csrf-token':
+              '0001000000011ada49f1c3d31fed290f27d4b5eaa5fb3ae53156af4d3893d218bea6aaec53e6175392e14eed34c2',
+          },
+          referrer: 'https://juejin.cn/',
+          referrerPolicy: 'strict-origin-when-cross-origin',
+          body: `{"sort_type":4,"cursor":"${cursor}","limit":20,"user_id":"${slardarData.userId}"}`,
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'include',
+        }
+      )
+      .then((res) => res.json());
+  }
+
+  return {
+    async start() {
+      let state = false;
+      let arrList: any[] = [];
+      while (!state) {
+        await getLsit(cursor).then((res) => {
+          let { err_no: errNo, data } = res;
+          if (errNo === 0) {
+            cursor = res.cursor;
+            arrList.push(...data);
+            state = data.some((v: any) => Number(v.msg_Info.ctime) < 1680451200);
+          }
+        });
+      }
+      // 过滤无效数据，action = 2 为 沸点数据
+      arrList = arrList.filter((v: any) => Number(v.msg_Info.ctime) >= 1680451200);
+      console.log(arrList.length);
+      arrList.forEach((v: any) => {
+        // console.log(v);
+        let dateUnix = dayjs(Number(v.msg_Info.ctime * 1000))
+          .startOf('day')
+          .unix();
+        let item = clockInData.find((v) => v.dateUnix === dateUnix);
+        if (item && item.check !== 1) {
+          if (item.topicList.find(v2=>v2.id === v.topic.topic_id)) {
+            item.check = 1;
+          }
+        }
+      });
+      return clockInData;
+    },
+    stop() { },
+  };
+}
